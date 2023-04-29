ec882b76d314f301817f7670168b39132b686a01
thomas <t.kinderman01@ufromail.cl>
2023-04-29 02:16:03 -0400

    commit

--- src/main/java/Cafe.java
+++ src/main/java/Cafe.java
@@ -1,2 +1,36 @@
 public class Cafe {
+    private double gramosCafe;
+    private double mililitrosAgua;
+    private String tamano;
+
+    public Cafe(double gramosCafe, double mililitrosAgua, String tamano) {
+        this.gramosCafe = gramosCafe;
+        this.mililitrosAgua = mililitrosAgua;
+        this.tamano = tamano;
+
+    }
+
+    public Cafe() {
+    }
+
+    public double getGramosCafe() {
+        return gramosCafe;
+    }
+
+    public double getMililitrosAgua() {
+        return mililitrosAgua;
+    }
+
+    public String getTamano() {
+        return tamano;
+    }
+
+    @Override
+    public String toString() {
+        return "Cafe{" +
+                "gramosCafe=" + gramosCafe +
+                ", mililitrosAgua=" + mililitrosAgua +
+                ", tamano='" + tamano + '\'' +
+                '}';
+    }
 }
--- src/main/java/Cafeteria.java
+++ src/main/java/Cafeteria.java
@@ -1,2 +1,54 @@
+import java.util.ArrayList;
+
 public class Cafeteria {
+    private String nombre;
+    private String direccion;
+    private ArrayList<String> redesSociales;
+    private ArrayList<Cafe> listaCafes;
+
+    public Cafeteria(String nombre, String direccion, ArrayList<String> redesSociales, ArrayList<Cafe> listaCafes) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.redesSociales = redesSociales;
+        this.listaCafes = listaCafes;
+    }
+
+    public Cafeteria() {
+    }
+
+    public ArrayList<Cafe> getListaCafes() {
+        return listaCafes;
+    }
+
+    public void setListaCafes(ArrayList<Cafe> listaCafes) {
+        this.listaCafes = listaCafes;
+    }
+
+    public Cafe agregarCafe(Cafe cafe){
+        listaCafes.add(cafe);
+        return cafe;
+    }
+
+    public int eliminarCafe(int indice){
+        listaCafes.remove(indice);
+        return indice;
+    }
+
+    public ArrayList<Cafe> buscarCafe(String tamano){
+        ArrayList<Cafe> cafes = new ArrayList<>();
+        for (Cafe cafe:listaCafes) {
+            cafes.add(cafe);
+        }
+        return cafes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cafeteria{" +
+                "nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", redesSociales=" + redesSociales +
+                ", listaCafes=" + listaCafes +
+                '}';
+    }
 }
